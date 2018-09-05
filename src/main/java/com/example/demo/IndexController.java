5abaec2042b74595bccf8fc6aeb63ab65de3fd97
andervilo <andervilo@hotmail.com>
2018-09-05 18:19:50 -0300

    .

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,22 +1,6 @@
+package com.example.demo;
 
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -31,7 +15,7 @@ public class IndexController {
 	
 	@GetMapping("/convidados")
 	public String convidados() {
-		return "Lista";
+		return "lista";
 	}
 	
 	
